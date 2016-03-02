c07a579cc880baf0246c62c8ede30fdb5b75259e
lion17s <serhii.sheptytskyi@gmail.com>
2016-03-02 12:59:37 +0200

    Added WebBrowser intit class

--- src/test/java/masterpages/PageHead.java
+++ src/test/java/masterpages/PageHead.java
@@ -2,20 +2,23 @@ package masterpages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 import utils.ComplexReportFactory;
 import utils.ExtentManager;
 import utils.SystemUtils;
+import utils.WebBrowser;
+
 import java.lang.reflect.Method;
 
 /**
  * Created by beetlezhuk on 11/19/15.
  */
-public class PageHead
+public class PageHead extends WebBrowser
 {
     protected SystemUtils utils;
     protected PagesFactory factory;
-    protected WebDriver driver;
+//    protected WebDriver driver;
     String test_title;
 
 //    @Parameters({"browser", "test_name"})
@@ -45,13 +48,14 @@ public class PageHead
 //        }
 //        else
 //        {
-            System.setProperty("webdriver.chrome.driver", utils.get_chrome_path());
-            this.driver = new ChromeDriver();
+   //         System.setProperty("webdriver.chrome.driver", utils.get_chrome_path());
+            //this.driver = new FirefoxDriver();
 //        }
 
         ComplexReportFactory.getTest(caller.getName(), "Very cool report");
-        this.factory = new PagesFactory(this.driver);
-        this.driver.manage().window().maximize();
+//        this.factory = new PagesFactory(this.driver);
+        this.factory = new PagesFactory(Driver());
+//      this.driver.manage().window().maximize();
 //        entered_values = new EnteredTestData();
 //        read_keywords = new ReadExcellKeywords(driver, entered_values);
     }
---
+++ src/test/java/utils/WebBrowser.java
@@ -0,0 +1,63 @@
+package utils;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+/**
+ * Created by Sergey S on 11/19/15.
+ */
+
+public class WebBrowser {
+
+	private static ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<WebDriver>();
+
+	private WebDriver driver = threadLocalDriver.get();
+
+	@Parameters("browser")
+	@BeforeTest
+	public void initWebBrowser(@Optional(value = "Firefox") String browser) {
+						
+	
+		if (browser.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+			System.out.println("Firefox has started");
+		} else if (browser.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
+			driver = new ChromeDriver();
+			System.out.println("Chrome has started");
+		} else if (browser.equalsIgnoreCase("IE")) {
+			System.setProperty("webdriver.ie.driver", "./resources/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		} else if (browser.equalsIgnoreCase("Opera")) {
+			System.setProperty("webdriver.opera.driver", "./resources/operadriver.exe");
+			driver = new OperaDriver();
+		}
+		threadLocalDriver.set(driver);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	public static WebDriver Driver() {	
+		return threadLocalDriver.get();
+	}
+	 
+	
+	@AfterTest(alwaysRun = true)
+	public void closeWebBrowser() {
+		if (driver != null) {
+			driver.quit();
+			threadLocalDriver.remove();
+			System.out.println("Browser closed");
+		}
+	}
+
+	
+}
